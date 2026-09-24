cd28c6b051ea6278276525b9d68389dcee39ffe9
Moritisimor <devMoritisimor@proton.me>
2026-09-24 22:50:26 +0200

    Added basic HttpClient and SendButton functionality

--- Thor/MainWindow.axaml.cs
+++ Thor/MainWindow.axaml.cs
@@ -1,12 +1,32 @@
+using System;
+using System.Net.Http;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace Thor;
 
 public partial class MainWindow : Window
 {
+    private readonly HttpClient _httpClient;
+    
     public MainWindow()
     {
         InitializeComponent();
         MethodTextBox.Text = "GET";
+        _httpClient = new HttpClient();
+    }
+
+    private async void SendButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+        try
+        {
+            var request = new HttpRequestMessage(HttpMethod.Parse(MethodTextBox.Text), UrlTextBox.Text);
+            var response = await _httpClient.SendAsync(request);
+            Console.WriteLine($"Status: {response.StatusCode}, Body: {response.Content.ReadAsStringAsync().Result}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
 }
