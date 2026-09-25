d6cec8ca2cbbace793b8a8fd2b43575ce54779b8
Moritisimor <devMoritisimor@proton.me>
2026-09-25 18:43:30 +0200

    Major update
    Added headers, implemented body functionality and added content-type text field

--- Thor/MainWindow.axaml.cs
+++ Thor/MainWindow.axaml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 
@@ -12,8 +13,8 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        MethodTextBox.Text = "GET";
         _httpClient = new HttpClient();
+        HeadersListBox.Items.Add("User-Agent: Thor");
     }
 
     private async void SendButton_OnClick(object? sender, RoutedEventArgs e)
@@ -21,14 +22,51 @@ public partial class MainWindow : Window
         try
         {
             var request = new HttpRequestMessage(HttpMethod.Parse(MethodTextBox.Text), UrlTextBox.Text);
+            request.Content = new StringContent(BodyTextBox?.Text ?? "");
+            var contentTypeHeader = new MediaTypeHeaderValue(ContentTypeTextBox.Text ?? "text/plain");
+            request.Content.Headers.ContentType = contentTypeHeader;
+            
             var response = await _httpClient.SendAsync(request);
             var resultWindow = new ResultWindow(response);
-            await resultWindow.ShowDialog(this);
+            resultWindow.Show(this);
         }
         catch (Exception ex)
         {
             var errorWindow = new ErrorWindow(ex.GetType().ToString(), ex.Message);
             await errorWindow.ShowDialog(this);
         }
+    }
+
+    private async void AddHeaderButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+        try
+        {
+            if (string.IsNullOrWhiteSpace(HeaderKeyTextBox.Text))
+                throw new InvalidOperationException("Bad Header key!");
+            
+            if (string.IsNullOrWhiteSpace(HeaderValueTextBox.Text))
+                throw new InvalidOperationException("Bad Header value!");
+            
+            if (HeaderKeyTextBox.Text.Contains(' ') 
+                || HeaderKeyTextBox.Text.Contains('\t')
+                || HeaderKeyTextBox.Text.Contains('\n'))
+                throw new InvalidOperationException("Header keys may not contain whitespace");
+
+            HeadersListBox.Items.Add($"{HeaderKeyTextBox.Text.Trim()}: {HeaderValueTextBox.Text.Trim()}");
+        }
+        catch (Exception ex)
+        {
+            var errorWindow = new ErrorWindow(ex.GetType().ToString(), ex.Message);
+            await errorWindow.ShowDialog(this);
+        }
+    }
+
+    private void DeleteHeaderButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (HeadersListBox.SelectedItem is not string item) 
+            return;
+        
+        HeadersListBox.Items.Remove(item);
+        HeadersListBox.SelectedItem = null;
     }
 }
